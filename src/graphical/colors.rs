d98b8be2d192873271dfd772093c5f7ff787ea9f
Remesk1 <eeliremes@gmail.com>
2026-05-24 09:58:49 +0300

    color tweaks

--- src/graphical/colors.rs
+++ src/graphical/colors.rs
@@ -2,9 +2,9 @@ use ratatui::style::Color;
 
 pub const PRIMARY: Color = Color::Rgb(150, 0, 0);
 pub const FG_TEXT: Color = Color::Rgb(255, 255, 255);
-pub const BG: Color = Color::Rgb(10, 10, 10);
-pub const BG_CONT: Color = Color::Rgb(10, 10, 10);
-pub const BORDER: Color = Color::Rgb(20, 20, 20);
+pub const BG: Color = Color::Rgb(15, 10, 10);
+pub const BG_CONT: Color = BG;
+pub const BORDER: Color = Color::Rgb(40, 40, 40);
 
 pub const SUCCESS: Color = Color::Rgb(20, 100, 0);
 pub const ERROR: Color = Color::Rgb(200, 0, 0);
