b6b67615783d16e211d451026bed242b78860573
Remesk1 <eeliremes@gmail.com>
2026-06-02 21:07:33 +0300

    fixed my oopsie

---
+++ src/integrations/beszel/api.rs
@@ -0,0 +1,127 @@
+use color_eyre::{Result, eyre::eyre};
+use reqwest::{RequestBuilder, Url, header::AUTHORIZATION, multipart::Form};
+
+use crate::integrations::beszel::records::{self, Container, List, System, SystemStats};
+
+#[derive(Debug)]
+pub struct Client {
+    http_handler: HTTPHandler,
+}
+
+impl Client {
+    pub fn new(base_url: Url) -> Self {
+        return Self {
+            http_handler: HTTPHandler::new(base_url, None),
+        };
+    }
+
+    pub async fn connect_auth_password(
+        &mut self,
+        identity: String,
+        password: String,
+    ) -> Result<()> {
+        self.http_handler.authenticate(identity, password).await?;
+        Ok(())
+    }
+
+    pub async fn systems(&self) -> Result<List<System>> {
+        let rb = self
+            .http_handler
+            .get("collections/systems/records")?;
+        let js = rb.send().await?.json::<List<System>>().await?;
+        Ok(js)
+    }
+
+    pub async fn system(&self, system_name: &str) -> Result<List<SystemStats>> {
+        let rb = self
+            .http_handler
+            .get("collections/system_stats/records")?
+            .query(&[
+                (
+                    "filter",
+                    format!("(system.name='{}')", system_name).as_str(),
+                ),
+                ("sort", "-created"),
+                ("perPage", "100"),
+            ]);
+        let js = rb.send().await?.json::<List<SystemStats>>().await?;
+
+        return Ok(js);
+    }
+
+    pub async fn containers(&self, system_name: &str) -> Result<List<Container>> {
+        let rb = self
+            .http_handler
+            .get("collections/containers/records")?
+            .query(&[
+                (
+                    "filter",
+                    format!("(system.name='{}')", system_name).as_str(),
+                ),
+                ("sort", "cpu"),
+            ]);
+        let js = rb.send().await?.json::<List<Container>>().await?;
+        Ok(js)
+    }
+
+    pub async fn containers_all(&self) -> Result<List<Container>> {
+        let rb = self
+            .http_handler
+            .get("collections/containers/records")?
+            .query(&[("sort", "-cpu")]);
+        let js = rb.send().await?.json::<List<Container>>().await?;
+        Ok(js)
+    }
+}
+
+#[derive(Debug)]
+struct HTTPHandler {
+    token: Option<String>,
+    base_url: Url,
+}
+
+#[allow(dead_code)]
+impl HTTPHandler {
+    fn new(base_url: Url, token: Option<String>) -> Self {
+        Self {
+            token,
+            base_url: base_url.join("api/").expect("URL problem"),
+        }
+    }
+
+    async fn authenticate(&mut self, identity: String, password: String) -> Result<()> {
+        let client = reqwest::Client::new()
+            .post(self.base_url.join("collections/users/auth-with-password")?);
+        let mp = client.multipart(
+            Form::new()
+                .text("identity", identity)
+                .text("password", password),
+        );
+        let resp = mp.send().await?;
+        let js = resp.json::<records::Auth>().await?;
+
+        self.token = Some(js.token);
+
+        Ok(())
+    }
+
+    fn get(&self, path: &str) -> Result<RequestBuilder> {
+        let client = reqwest::Client::new().get(self.base_url.join(path)?);
+        if let Some(token) = &self.token {
+            let rb = client.header(AUTHORIZATION, token.as_str());
+            return Ok(rb);
+        } else {
+            return Err(eyre!("unauthorized! token missing"));
+        }
+    }
+
+    fn post(&self, path: &str) -> Result<RequestBuilder> {
+        let client = reqwest::Client::new().post(self.base_url.join(path)?);
+        if let Some(token) = &self.token {
+            let rb = client.header(AUTHORIZATION, token.as_str());
+            return Ok(rb);
+        } else {
+            return Err(eyre!("unauthorized! token missing"));
+        }
+    }
+}
